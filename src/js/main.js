db48488c67e0a1fb099f45c2bacd6e2b42030669
Gustavo Rosa <gth.rosa@uol.com.br>
2017-05-16 12:38:02 -0400

    Bundling up some js files.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+// Flags-Icon JS
+window.onload = function () {
+  $('.all-flags .flag-icon-background').click(function(event){
+    var flag = $(event.currentTarget).attr('title');
+    var w = 640;
+    var h = 480;
+    var left = (screen.width / 2) - (w / 2);
+    var top = (screen.height / 2) - (h / 2);
+
+    if (flag) {
+      window.open('flags/4x3/' + flag + '.svg', 'flag-4x3', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    }
+  });
+}
+
 // Create language switcher instance
 var lang = new Lang();
 lang.dynamic('pt', 'js/langpack/pt.json');
