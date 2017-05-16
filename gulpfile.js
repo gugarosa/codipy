ff0f161596ed9c07107c43fde5d6c97b2da7ba6b
Gustavo Rosa <gth.rosa@uol.com.br>
2017-05-16 11:13:51 -0400

    Initial files commit.

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  cleancss = require('gulp-clean-css'),
+  connect = require('gulp-connect'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename');
+
+var jsSource = ['src/js/*.js'],
+  sassSource = ['src/style.scss', 'src/scss/*.scss'],
+  htmlSource = ['src/*.html'];
+
+
+gulp.task('log', function () {
+  return gutil.log('Initializing gulp...');
+});
+
+gulp.task('copy', function () {
+  gulp.src('src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+  gulp.src('src/flags/**')
+    .pipe(gulp.dest('dist/flags'))
+  gulp.src('src/js/langpack/*')
+    .pipe(gulp.dest('dist/js/langpack'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(sassSource[0])
+    .pipe(sass({ style: 'expanded' }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('minify', ['sass'], function () {
+  return gulp.src('dist/css/style.css')
+    .pipe(cleancss({ compatibility: 'ie8' }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  return gulp.src(jsSource)
+    .pipe(uglify())
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function() {
+  return gulp.src(htmlSource)
+  .pipe(gulp.dest('dist'))
+  .pipe(connect.reload());
+});
+
+gulp.task('connect', function () {
+  return connect.server({
+    root: './dist',
+    livereload: true
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(jsSource, ['js']);
+  gulp.watch(sassSource, ['minify']);
+  gulp.watch(htmlSource, ['html']);
+});
+
+gulp.task('default', ['copy', 'js', 'minify', 'html', 'connect', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// Create language switcher instance
+var lang = new Lang();
+lang.dynamic('pt', 'js/langpack/pt.json');
+lang.init({
+    defaultLang: 'en'
+});
